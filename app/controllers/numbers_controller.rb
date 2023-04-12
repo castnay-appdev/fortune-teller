5e1d9a1c6ff8f290951ee2be902705aa9ef21708
Nayla Castillo <naylac14@gmail.com>
2023-04-12 21:40:01 +0000

    Fortune Teller Debugged

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,16 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def losers
+    @dog = Array.new
+
+    5.times do
+      horse = rand(1...100)
+      
+      @dog.push(horse)
+    end
+
+    render({ :template => "lottery_stuff/boo.html.erb"})
+  end 
 end
